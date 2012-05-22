def25edc8d2c0bfb98ad36d5491a1fb5818834a6
Michael Driscoll <mbdriscoll@gmail.com>
2012-05-21 23:50:15 -0700

    got gasnet_init working, or at least not segfaulting

--- Modules/gasnet.c
+++ Modules/gasnet.c
@@ -6,18 +6,41 @@
 static PyObject *
 _gasnet_init(PyObject *self, PyObject *args)
 {
-    return Py_BuildValue("i", 42);
+    int argc = 2;
+    char **argv = (char**) malloc(argc * sizeof(char*));
+    argv[0] = "./pygas";
+    argv[1] = "1";
+
+    int status = gasnet_init(&argc, &argv);
+    return Py_BuildValue("i", status);
 }
 
 static PyObject *
 _gasnet_exit(PyObject *self, PyObject *args)
 {
-    return Py_BuildValue("i", 42);
+    gasnet_exit(0);
+    return Py_None;
+}
+
+static PyObject *
+_gasnet_mynode(PyObject *self, PyObject *args)
+{
+    gasnet_node_t rank = gasnet_mynode();
+    return Py_BuildValue("i", rank);
+}
+
+static PyObject *
+_gasnet_nodes(PyObject *self, PyObject *args)
+{
+    gasnet_node_t ranks = gasnet_nodes();
+    return Py_BuildValue("i", ranks);
 }
 
 static PyMethodDef gasnet_methods[] = {
-    {"init",    _gasnet_init, METH_VARARGS, "Initialize GASNet runtime."},
-    {"exit",    _gasnet_exit, METH_VARARGS, "Terminate GASNet runtime."},
+    {"init",    _gasnet_init,    METH_VARARGS, "Initialize GASNet runtime."},
+    {"exit",    _gasnet_exit,    METH_VARARGS, "Terminate GASNet runtime."},
+    {"nodes",   _gasnet_nodes,   METH_VARARGS, "Number of nodes in job."},
+    {"mynode",  _gasnet_mynode,  METH_VARARGS, "Index of current node in job."},
     {NULL,      NULL}           /* sentinel */
 };
 
