a28c458abf58d9e19928d82c1c92fd0b451caf14
Michael Driscoll <mbdriscoll@gmail.com>
2012-05-24 23:35:49 -0700

    move imports to the top, clean up a bit

--- Lib/pygas.py
+++ Lib/pygas.py
@@ -1,14 +1,25 @@
 import _gasnet as gasnet
 
+import atexit   # for registering gasnet_exit at termination
+import warnings # for warn
+import operator # for add (default reduction operation)
+
 gasnet.init()
 gasnet.attach()
 
 THREADS = gasnet.nodes()
 MYTHREAD = gasnet.mynode()
 
-import atexit
 atexit.register(gasnet.exit)
 
 def barrier(id=0,flags=gasnet.BARRIERFLAG_ANONYMOUS):
   gasnet.barrier_notify(id,flags)
   gasnet.barrier_wait(id,flags)
+
+def shared(obj):
+  warnings.warn("shared() not implemented")
+  return obj
+
+def all_reduce(obj, fxn=operator.add):
+  warnings.warn("all_reduce() not implemented")
+  return fxn(obj,obj)
