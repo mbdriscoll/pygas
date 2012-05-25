4a2e4237bcd6cbb22319d55c7cefe9d16246e59b
Michael Driscoll <mbdriscoll@gmail.com>
2012-05-24 22:50:06 -0700

    called gasnet_exit upon termination of interpreter

--- Lib/pygas.py
+++ Lib/pygas.py
@@ -6,6 +6,9 @@ gasnet.attach()
 THREADS = gasnet.nodes()
 MYTHREAD = gasnet.mynode()
 
+import atexit
+atexit.register(gasnet.exit)
+
 def barrier(id=0,flags=gasnet.BARRIERFLAG_ANONYMOUS):
   gasnet.barrier_notify(id,flags)
   gasnet.barrier_wait(id,flags)
