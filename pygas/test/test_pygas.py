fa6ace7c5069f09665cd93b5115695cccd294583
Michael Driscoll <mbdriscoll@gmail.com>
2012-06-05 23:08:54 +0000

    use self.assert

--- pygas/test/test_pygas.py
+++ pygas/test/test_pygas.py
@@ -6,15 +6,15 @@ class TestNames(unittest.TestCase):
 
     def test_MYTHREAD_constant(self):
         from pygas import MYTHREAD
-        unittest.assertGreaterEqual(MYTHREAD, 0)
+        self.assertGreaterEqual(MYTHREAD, 0)
 
     def test_THREADS_constant(self):
         from pygas import THREADS
-        unittest.assertGreaterEqual(THREADS, 0)
+        self.assertGreaterEqual(THREADS, 0)
 
     def test_threadid_bounds(self):
         from pygas import THREADS, MYTHREAD
-        unittest.assertLess(MYTHREAD, THREADS)
+        self.assertLess(MYTHREAD, THREADS)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestNames)
 
