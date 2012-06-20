4c7d437ca7bfb49a80d34e4868f775439c0f2750
Michael Driscoll <mbdriscoll@gmail.com>
2012-06-20 14:08:19 -0700

    clean up setup.py file. still not like autoconf but closer

--- setup.py
+++ setup.py
@@ -1,20 +1,64 @@
 import sys
+from os import path
 from distutils.core import setup, Extension
+from distutils.errors import DistutilsSetupError
+from distutils import sysconfig
 
-# TODO figure out the equivalent of ./configure ...
-if sys.platform.startswith('linux'):
-  GASNET_PATH='/ebs/opt/gasnet-1.18.2'
-  MPI_PATH='/ebs/opt/mpich2-1.4.1'
-elif sys.platform.startswith('darwin'):
-  GASNET_PATH='/Users/mbdriscoll/opt/gasnet-1.18.2'
-  MPI_PATH='/Users/mbdriscoll/opt/mpich2-1.4.1'
+# TODO cmd-line or environ config ability (--with-gasnet=/path/to/gasnet)
+GASNET_PATH    = '/ebs/opt/gasnet-1.18.2'
+GASNET_CONDUIT = 'mpi'
+MPI_PATH       = '/usr/lib/mpich2'
 
+# Only allow supported conduits. Supporting more conduits is a matter of
+# updating this script to configure them properly.
+if GASNET_CONDUIT != 'mpi':
+    raise DistutilsSetupError("Unsupported conduit: %s", GASNET_CONDUIT)
+
+# Sanity check gasnet installation.
+gasnet_h_path = path.join(GASNET_PATH, "include", "gasnet.h")
+if not path.exists(gasnet_h_path):
+    raise DistutilsSetupError("Cannot locate %s." % gasnet_h_path)
+del gasnet_h_path
+
+# Sanity check gasnet-conduit installation.
+gasnet_core_h_path = path.join(GASNET_PATH, "include", "%s-conduit" % GASNET_CONDUIT, "gasnet_core.h")
+if not path.exists(gasnet_core_h_path):
+    raise DistutilsSetupError("Cannot locate %s." % gasnet_core_h_path)
+del gasnet_core_h_path
+
+# Sanity check MPI installation.
+mpi_h_path = path.join(MPI_PATH, "include", "mpi.h")
+if not path.exists(mpi_h_path):
+    raise DistutilsSetupError("Cannot locate %s." % mpi_h_path)
+del mpi_h_path
+
+# Set up include dirs
+include_dirs = [
+    path.join(GASNET_PATH, 'include'),
+    path.join(GASNET_PATH, 'include', 'mpi-conduit'),
+]
+
+# Set up library dirs
+library_dirs = [
+    path.join(GASNET_PATH, 'lib'),
+    path.join(MPI_PATH, 'lib'),
+]
+
+# Set up libraries
 libraries = ['gasnet-mpi-par', 'ammpi', 'mpich', 'mpl']
 if sys.platform.startswith('linux'):
   libraries += ['rt']
 elif sys.platform.startswith('darwin'):
   libraries += ['pmpich']
 
+# Set up define_macros
+define_macros = [
+   ('GASNET_ALLOW_OPTIMIZED_DEBUG', 1),
+   ('GASNETT_USE_GCC_ATTRIBUTE_MAYALIAS', 1),
+   ('DEBUG', 1),
+]
+
+# Run setup.
 setup(
   name='PyGAS',
   version='0.95',
@@ -22,25 +66,14 @@ setup(
   author='Michael Driscoll',
   author_email='mbdriscoll@cs.berkeley.edu',
   url='http://github.com/mbdriscoll/pygas',
-  packages=['pygas', 'pygas.robj', 'pygas.test'],
+  packages=['pygas', 'pygas.test'],
   ext_modules=[
     Extension('pygas.gasnet',
       ['pygas/gasnet/gasnet.c'],
-      include_dirs=[
-        GASNET_PATH+'/include',
-        GASNET_PATH+'/include/mpi-conduit',
-        MPI_PATH+'/include',
-      ],
-      library_dirs=[
-        GASNET_PATH+'/lib',
-        MPI_PATH+'/lib',
-      ],
+      include_dirs=include_dirs,
+      library_dirs=library_dirs,
       libraries=libraries,
-      define_macros=[
-        ('GASNET_ALLOW_OPTIMIZED_DEBUG', 1),
-        ('GASNETT_USE_GCC_ATTRIBUTE_MAYALIAS', 1),
-        ('DEBUG', 1),
-      ],
+      define_macros=define_macros,
     ),
   ]
 )
