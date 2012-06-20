ee27e2e3db8bae4033160c61237f3feba38e1c52
Michael Driscoll <mbdriscoll@gmail.com>
2012-06-20 12:33:55 -0700

    use pygas_* namespace for c entities. general cleanup

--- pygas/gasnet/gasnet.c
+++ pygas/gasnet/gasnet.c
@@ -12,7 +12,7 @@
 // adding new hidx? be sure to update gasnet_handerentry_t count
 
 // TODO check implications of this with gasnet team
-#define PYGASNET_BLOCKUNTIL(cond) \
+#define PYGAS_GASNET_BLOCKUNTIL(cond) \
     while (!(cond)) {             \
         gasnet_AMPoll();          \
 	Py_MakePendingCalls();    \
@@ -23,7 +23,7 @@
  * interpreter while waiting.
  * XXX: spins. check with gasnet team for advice here.
  * TODO: support for other gasnet_barrier_try return codes. */
-#define PYGASNET_BARRIER_WAIT(id, flags) \
+#define PYGAS_GASNET_BARRIER_WAIT(id, flags) \
     while (gasnet_barrier_try((id), (flags)) != GASNET_OK) { \
         Py_MakePendingCalls(); \
     }
@@ -31,11 +31,11 @@
 typedef struct msg_info {
     gasnet_node_t sender;
     size_t nbytes;
-    void* addr;
+    void *addr;
 } msg_info_t;
   
 static PyObject *
-py_gasnet_init(PyObject *self, PyObject *args)
+pygas_gasnet_init(PyObject *self, PyObject *args)
 {
     PyEval_InitThreads();
 
@@ -49,7 +49,7 @@ py_gasnet_init(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_apply_dynamic(PyObject *self, PyObject *args)
+pygas_gasnet_apply_dynamic(PyObject *self, PyObject *args)
 {
     char *data;
     int dest = 0, nbytes = 0;
@@ -57,17 +57,17 @@ py_gasnet_apply_dynamic(PyObject *self, PyObject *args)
 
     char *reply = NULL;
     char request[sizeof(msg_info_t)+nbytes];
-    msg_info_t* request_info = (msg_info_t*) &request[0];
+    msg_info_t *request_info = (msg_info_t*) &request[0];
     request_info->sender = MYTHREAD;
     request_info->nbytes = nbytes;
     request_info->addr = (void*) &reply;
     memcpy(&request[sizeof(msg_info_t)], data, nbytes);
 
     gasnet_AMRequestMedium0(dest, APPLY_DYNAMIC_REQUEST_HIDX, &request, sizeof(request));
-    PYGASNET_BLOCKUNTIL(reply != NULL);
+    PYGAS_GASNET_BLOCKUNTIL(reply != NULL);
 
-    msg_info_t* reply_info = (msg_info_t*) &reply[0];
-    PyObject * result = Py_BuildValue("s#", &reply[sizeof(msg_info_t)], reply_info->nbytes);
+    msg_info_t *reply_info = (msg_info_t*) &reply[0];
+    PyObject *result = Py_BuildValue("s#", &reply[sizeof(msg_info_t)], reply_info->nbytes);
 
     free(reply);
     return result;
@@ -122,7 +122,7 @@ pygas_async_request_handler(char* request) {
 void
 pygas_apply_dynamic_request_handler(gasnet_token_t token, char* data, size_t nbytes)
 {
-    char* msg = (char*) malloc(nbytes);
+    char *msg = (char*) malloc(nbytes);
     memcpy(msg, data, nbytes);
     Py_AddPendingCall(pygas_async_request_handler, msg);
 }
@@ -130,10 +130,10 @@ pygas_apply_dynamic_request_handler(gasnet_token_t token, char* data, size_t nby
 void
 pygas_apply_dynamic_reply_handler(gasnet_token_t token, void* data, size_t nbytes)
 {
-    char* reply = (char*) malloc(nbytes);
+    char *reply = (char*) malloc(nbytes);
     memcpy(reply, data, nbytes);
 
-    // write val to end PYGASNET_BLOCKUNTIL
+    // write val to end PYGAS_GASNET_BLOCKUNTIL
     msg_info_t* reply_info = (msg_info_t*) &reply[0];
     *((char**) reply_info->addr) = reply;
 }
@@ -144,7 +144,7 @@ gasnet_handlerentry_t handler_table[] = {
 };
 
 static PyObject *
-py_gasnet_attach(PyObject *self, PyObject *args)
+pygas_gasnet_attach(PyObject *self, PyObject *args)
 {
     int status = gasnet_attach(&handler_table, 2, gasnet_getMaxLocalSegmentSize(), GASNET_PAGESIZE);
 
@@ -152,7 +152,7 @@ py_gasnet_attach(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_exit(PyObject *self, PyObject *args)
+pygas_gasnet_exit(PyObject *self, PyObject *args)
 {
     int ok;
     int exitcode = 0;
@@ -160,7 +160,7 @@ py_gasnet_exit(PyObject *self, PyObject *args)
 
     /* execute barrier to be compliant with spec */
     gasnet_barrier_notify(0, GASNET_BARRIERFLAG_ANONYMOUS);
-    PYGASNET_BARRIER_WAIT(0, GASNET_BARRIERFLAG_ANONYMOUS);
+    PYGAS_GASNET_BARRIER_WAIT(0, GASNET_BARRIERFLAG_ANONYMOUS);
 
     gasnet_exit(exitcode);
 
@@ -168,21 +168,21 @@ py_gasnet_exit(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_mynode(PyObject *self, PyObject *args)
+pygas_gasnet_mynode(PyObject *self, PyObject *args)
 {
     gasnet_node_t rank = gasnet_mynode();
     return Py_BuildValue("i", rank);
 }
 
 static PyObject *
-py_gasnet_nodes(PyObject *self, PyObject *args)
+pygas_gasnet_nodes(PyObject *self, PyObject *args)
 {
     gasnet_node_t ranks = gasnet_nodes();
     return Py_BuildValue("i", ranks);
 }
 
 static PyObject *
-py_gasnet_getenv(PyObject *self, PyObject *args)
+pygas_gasnet_getenv(PyObject *self, PyObject *args)
 {
     int ok;
     const char *key, *value;
@@ -194,20 +194,20 @@ py_gasnet_getenv(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_barrier_wait(PyObject *self, PyObject *args)
+pygas_gasnet_barrier_wait(PyObject *self, PyObject *args)
 {
     int ok;
     int id = 0;
     int flags = GASNET_BARRIERFLAG_ANONYMOUS;
     ok = PyArg_ParseTuple(args, "|ii", &id, &flags);
 
-    PYGASNET_BARRIER_WAIT(id, flags);
+    PYGAS_GASNET_BARRIER_WAIT(id, flags);
 
     Py_RETURN_NONE;
 }
 
 static PyObject *
-py_gasnet_barrier_notify(PyObject *self, PyObject *args)
+pygas_gasnet_barrier_notify(PyObject *self, PyObject *args)
 {
     int ok;
     int id = 0;
@@ -220,7 +220,7 @@ py_gasnet_barrier_notify(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_barrier_try(PyObject *self, PyObject *args)
+pygas_gasnet_barrier_try(PyObject *self, PyObject *args)
 {
     int ok;
     int id = 0;
@@ -233,7 +233,7 @@ py_gasnet_barrier_try(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_coll_init(PyObject *self, PyObject *args)
+pygas_gasnet_coll_init(PyObject *self, PyObject *args)
 {
     int ok;
     ok = PyArg_ParseTuple(args, "");
@@ -244,7 +244,7 @@ py_gasnet_coll_init(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_gasnet_coll_broadcast(PyObject *self, PyObject *args)
+pygas_gasnet_coll_broadcast(PyObject *self, PyObject *args)
 {
     int from_thread = 0;
     Py_buffer pb;
@@ -260,7 +260,7 @@ py_gasnet_coll_broadcast(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_obj_to_capsule(PyObject *self, PyObject *args)
+pygas_obj_to_capsule(PyObject *self, PyObject *args)
 {
     int ok;
     PyObject *obj;
@@ -273,10 +273,10 @@ py_obj_to_capsule(PyObject *self, PyObject *args)
 }
 
 static PyObject *
-py_capsule_to_obj(PyObject *self, PyObject *args)
+pygas_capsule_to_obj(PyObject *self, PyObject *args)
 {
     long ptr;
-    PyObject* obj;
+    PyObject *obj;
     assert(PyArg_ParseTuple(args, "l", &ptr));
 
     obj = (PyObject*) ptr;
@@ -284,26 +284,26 @@ py_capsule_to_obj(PyObject *self, PyObject *args)
     return obj;
 }
     
-static PyMethodDef py_gasnet_methods[] = {
-    {"init",           py_gasnet_init,           METH_VARARGS, "Bootstrap GASNet job."},
-    {"exit",           py_gasnet_exit,           METH_VARARGS, "Terminate GASNet runtime."},
-    {"attach",         py_gasnet_attach,         METH_NOARGS,  "Initialize and setup node."},
-    {"nodes",          py_gasnet_nodes,          METH_VARARGS, "Number of nodes in job."},
-    {"mynode",         py_gasnet_mynode,         METH_VARARGS, "Index of current node in job."},
-    {"getenv",         py_gasnet_getenv,         METH_VARARGS, "Query environment when job was spawned."},
-    {"barrier_notify", py_gasnet_barrier_notify, METH_VARARGS, "Execute notify for split-phase barrier."},
-    {"barrier_wait",   py_gasnet_barrier_wait,   METH_VARARGS, "Execute wait for split-phase barrier."},
-    {"barrier_try",    py_gasnet_barrier_try,    METH_VARARGS, "Execute try for split-phase barrier."},
+static PyMethodDef pygas_gasnet_methods[] = {
+    {"init",           pygas_gasnet_init,           METH_VARARGS, "Bootstrap GASNet job."},
+    {"exit",           pygas_gasnet_exit,           METH_VARARGS, "Terminate GASNet runtime."},
+    {"attach",         pygas_gasnet_attach,         METH_NOARGS,  "Initialize and setup node."},
+    {"nodes",          pygas_gasnet_nodes,          METH_VARARGS, "Number of nodes in job."},
+    {"mynode",         pygas_gasnet_mynode,         METH_VARARGS, "Index of current node in job."},
+    {"getenv",         pygas_gasnet_getenv,         METH_VARARGS, "Query environment when job was spawned."},
+    {"barrier_notify", pygas_gasnet_barrier_notify, METH_VARARGS, "Execute notify for split-phase barrier."},
+    {"barrier_wait",   pygas_gasnet_barrier_wait,   METH_VARARGS, "Execute wait for split-phase barrier."},
+    {"barrier_try",    pygas_gasnet_barrier_try,    METH_VARARGS, "Execute try for split-phase barrier."},
 
     // Collectives. TODO refactor into separate file
-    {"coll_init",      py_gasnet_coll_init,      METH_VARARGS, "Initialize collectives."},
-    {"broadcast",      py_gasnet_coll_broadcast, METH_VARARGS, "Broadcast."},
+    {"coll_init",      pygas_gasnet_coll_init,      METH_VARARGS, "Initialize collectives."},
+    {"broadcast",      pygas_gasnet_coll_broadcast, METH_VARARGS, "Broadcast."},
 
     // My functions.
-    {"apply_dynamic",  py_gasnet_apply_dynamic,  METH_VARARGS, "Apply a dynamic function"},
+    {"apply_dynamic",  pygas_gasnet_apply_dynamic,  METH_VARARGS, "Apply a dynamic function"},
     {"set_apply_dynamic_handler",  set_apply_dynamic_handler,  METH_VARARGS, "Set the request hanlder that applies a dynamic function"},
-    {"obj_to_capsule",    py_obj_to_capsule,  METH_VARARGS, "Encapsulate the given object."},
-    {"capsule_to_obj",    py_capsule_to_obj,  METH_VARARGS, "Decapsulate the given capsule."},
+    {"obj_to_capsule",    pygas_obj_to_capsule,  METH_VARARGS, "Encapsulate the given object."},
+    {"capsule_to_obj",    pygas_capsule_to_obj,  METH_VARARGS, "Decapsulate the given capsule."},
 
     // Sentinel
     {NULL,             NULL}
@@ -312,9 +312,8 @@ static PyMethodDef py_gasnet_methods[] = {
 PyMODINIT_FUNC
 initgasnet(void)
 {
-    PyObject *module = Py_InitModule3("gasnet", py_gasnet_methods, "Interface to GASNet.");
+    PyObject *module = Py_InitModule3("gasnet", pygas_gasnet_methods, "Interface to GASNet.");
 
     PyModule_AddIntConstant(module, "BARRIERFLAG_ANONYMOUS", GASNET_BARRIERFLAG_ANONYMOUS);
     PyModule_AddIntConstant(module, "BARRIERFLAG_MISMATCH",  GASNET_BARRIERFLAG_MISMATCH);
-
 }
