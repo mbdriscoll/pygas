5addf45e59ec140d94f046628dfada7663998911
Michael Driscoll <mbdriscoll@gmail.com>
2012-06-20 17:12:47 -0700

    enable coverage and profiling plugins in nosetests

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ export OPT='-g'
 python setup.py build_ext --inplace --debug
 
 
-mpirun -n 4 -errfile-pattern=test-$DATE-%r.err -outfile-pattern=test-$DATE-%r.out nosetests -v
+mpirun -n 4 -errfile-pattern=test-$DATE-%r.err -outfile-pattern=test-$DATE-%r.out nosetests -v --with-coverage --with-profile
