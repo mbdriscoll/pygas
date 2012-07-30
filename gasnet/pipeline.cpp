8d00bd0f73025a7979e32eafc2d1936116328b57
Michael Driscoll <mbdriscoll@gmail.com>
2012-07-30 16:38:54 -0700

    cleanup round 1

--- gasnet/pipeline.cpp
+++ gasnet/pipeline.cpp
@@ -38,44 +38,49 @@ public:
         for(int i = 0; i < num_fragments; i++)
             this->fragments[i] = 1;
     }
+
+    char* slot_for_fragment_num(msg_info_t* frag_info) {
+        return this->data +
+               sizeof(msg_info_t) +
+               frag_info->fragment_num * PYGAS_MAX_PAYLOAD;
+    }
+
+    bool ready() {
+        return !this->fragments.any();
+    }
 };
 
 
 map<MsgID,MsgBuf*> recv_buf;
 
 /* Register a fragment of a message. If all fragments are in place,
- * return 1 and populate MSG and NBYTES with the message address
- * and size in bytes. Caller is responsible for freeing MSG on
- * success. */
+ * return 1 and populate MSG with the message address and size in
+ * bytes. Caller is responsible for freeing MSG on success. */
 int pygas_register_fragment(char* fragment, char** msg)
 {
     msg_info_t* frag_info = (msg_info_t*) &fragment[0];
-    MsgID mid(frag_info->sender, frag_info->addr);
 
-    // small message -> skip pipelining
+    // skip pipelining for small messages
     if (frag_info->nbytes == frag_info->total_bytes) {
         int frag_size = frag_info->nbytes + sizeof(msg_info_t);
         *msg = (char*) malloc(frag_size);
         memcpy(*msg, fragment, frag_size);
-        recv_buf.erase(mid);
         return 1;
     }
 
     MsgBuf* buf;
+    MsgID mid(frag_info->sender, frag_info->addr);
+
     if (recv_buf.find(mid) == recv_buf.end()) {
         buf = recv_buf[mid] = new MsgBuf(frag_info);
     } else {
         buf = recv_buf[mid];
     }
-    char* slot = buf->data +
-                 sizeof(msg_info_t) +
-                 frag_info->fragment_num * PYGAS_MAX_PAYLOAD;
+    char* slot = buf->slot_for_fragment_num(frag_info);
     memcpy(slot, fragment+sizeof(msg_info_t), frag_info->nbytes);
     buf->fragments[frag_info->fragment_num] = 0;
 
-    if (buf->fragments.any()) { // still waiting on fragments
-        return 0;
-    } else { // all fragments assembled
+    if (buf->ready()) {
         msg_info_t* msg_info = (msg_info_t*) buf->data;
         memcpy(msg_info, frag_info, sizeof(msg_info_t));
         msg_info->nbytes = frag_info->total_bytes;
@@ -83,6 +88,8 @@ int pygas_register_fragment(char* fragment, char** msg)
         recv_buf.erase(mid);
         *msg = buf->data;
         return 1;
+    } else {
+        return 0;
     }
 
     // todo free things ?
